afc7663dd3f0f8f78ae623950c1aeb50ef180f18
wvwall <waltervelardo@gmail.com>
2021-03-11 16:22:13 +0100

    Aggiunto scorrimento pallini + solito problema

--- main.js
+++ main.js
@@ -6,10 +6,13 @@ $(document).ready(function () {
         
         var imgInVista = $("img.active");
         imgInVista.removeClass("active");
+        var iInVista = $("i.active");
+        iInVista.removeClass("active");
         var prima = $("img.first");
         
         if(imgInVista.next("img")) {
             imgInVista.next("img").addClass("active");
+            iInVista.next("i").addClass("active");
         } else {
             prima.addClass("active");
             
@@ -22,10 +25,13 @@ $(document).ready(function () {
         
         var imgInVista = $("img.active");
         imgInVista.removeClass("active");
+        var iInVista = $("i.active");
+        iInVista.removeClass("active");
         var ultima = $("img.last");
         
         if(imgInVista.prev("img")) {
             imgInVista.prev("img").addClass("active");
+            iInVista.prev("i").addClass("active");
         } else {
            ultima.addClass("active");
             
