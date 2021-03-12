91b71ba83ccbaa5803e4241402c79ad7506811e1
wvwall <waltervelardo@gmail.com>
2021-03-12 09:16:22 +0100

    Scorrimento tastiera

--- main.js
+++ main.js
@@ -2,6 +2,7 @@ $(document).ready(function () {
 
     //SCORRERE A DX
 
+
     $(".fa-angle-right").click(function() {
         
         var imgInVista = $("img.active");
@@ -47,4 +48,52 @@ $(document).ready(function () {
     });
 
 
+    //SCORRERE CON FRECCE TASTIERA
+    
+    $(document.documentElement).keyup(function(e) {
+        
+        if(e.keyCode == 39) {
+            var imgInVista = $("img.active");
+        imgInVista.removeClass("active");
+
+        var iInVista = $("i.active");
+        iInVista.removeClass("active");
+        
+        var primaImg = $("img.first");
+        var primoPallino = $("i.first");
+        
+        if(imgInVista.next("img").length) {
+            imgInVista.next("img").addClass("active");
+            iInVista.next("i").addClass("active");
+        } else {
+            primaImg.addClass("active");
+            primoPallino.addClass("active");
+            
+        }
+        }
+        if(e.keyCode == 37) {
+            var imgInVista = $("img.active");
+        imgInVista.removeClass("active");
+
+        var iInVista = $("i.active");
+        iInVista.removeClass("active");
+
+        var ultimaImg = $("img.last");
+        var ultimoPallino = $("i.last");
+        
+        if(imgInVista.prev("img").length) {
+            imgInVista.prev("img").addClass("active");
+            iInVista.prev("i").addClass("active");
+        } else {
+           ultimaImg.addClass("active");
+           ultimoPallino.addClass("active");
+            
+        }
+        }
+    });
+
+
+
 });
+
+
